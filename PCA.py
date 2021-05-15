2d61b55df482d02513c90f09dd04893f872c3ae9
Ayla Bilgin <79978688+AylaBilgin@users.noreply.github.com>
2021-05-15 12:42:15 +0300

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,3 +1,5 @@
+#URL: https://www.datasciencearth.com/boyut-azaltma-mantigi-ve-ornek-bir-temel-bilesenler-analizi-uygulamasi/
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
